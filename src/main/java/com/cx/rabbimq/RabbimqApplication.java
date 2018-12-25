2876a28c201cabe08f4414e21e492a6c7b189a70
caixin <836566274@qq.com>
2018-12-25 16:36:56 +0800

    RabbiMQ  第一次上传

---
+++ src/main/java/com/cx/rabbimq/RabbimqApplication.java
@@ -0,0 +1,14 @@
+package com.cx.rabbimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbimqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbimqApplication.class, args);
+	}
+
+}
+
